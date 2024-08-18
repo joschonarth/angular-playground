6a84dbf75525a7845fe1b5655357365c9a866ebb
joschonarth <joschonarth@gmail.com>
2024-08-18 10:23:20 -0300

    Update Components

--- desafio-01/src/Components/CardNews.js
+++ desafio-01/src/Components/CardNews.js
@@ -19,6 +19,7 @@ class Cardnews extends HTMLElement {
 
         const linkTitle = document.createElement("a");
         linkTitle.textContent = this.getAttribute("title");
+        linkTitle.href = this.getAttribute("link-url");
 
         const newsContent = document.createElement("p");
         newsContent.textContent = this.getAttribute("content");
@@ -31,6 +32,8 @@ class Cardnews extends HTMLElement {
         cardRight.setAttribute("class", "card__right");
 
         const newsImage = document.createElement("img");
+        newsImage.src = this.getAttribute("photo") || "assets/default.jpg";
+        newsImage.alt = "Foto da Notícia";
 
         cardRight.appendChild(newsImage);
 
@@ -41,6 +44,54 @@ class Cardnews extends HTMLElement {
     }
 
     styles() {
+        const style = document.createElement("style");
+        style.textContent = `
+                * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: 'Segoe UI', sans-serif;
+            }
+
+            img {
+                height: 200px;
+                width: 400px
+            }
+
+            .card {
+                width: 80%;
+                display: flex;
+                flex-direction: row;
+                justify-content: space-between;
+            }
+
+            .card__left {
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                padding-left: 10px;
+            }
+
+            .card__left > span {
+                font-weight: 500;
+            }
+
+            .card__left > a {
+                margin-top: 15px;
+                font-size: 25px;
+                color: #333;
+                text-decoration: none;
+                font-weight: bold;
+                cursor: pointer;
+            }
+
+            .card__left > p {
+                color: rgb(70);
+            }
+
+        `;
+
+        return style;
 
     }
 }
