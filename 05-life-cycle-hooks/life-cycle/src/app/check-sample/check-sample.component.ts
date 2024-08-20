03809a1ed1b52ec50d1b3f9c87fc53517f07e991
joschonarth <joschonarth@gmail.com>
2024-08-19 23:25:06 -0300

    Update Angular Life Cycle Hooks

--- 05-life-cycle-hooks/life-cycle/src/app/check-sample/check-sample.component.ts
+++ 05-life-cycle-hooks/life-cycle/src/app/check-sample/check-sample.component.ts
@@ -1,32 +1,54 @@
-import { Component, OnInit, DoCheck, AfterContentChecked, AfterContentInit,AfterViewChecked, AfterViewInit } from '@angular/core';
+import { Component, OnInit, DoCheck, AfterContentChecked, AfterContentInit,AfterViewChecked, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-check-sample',
   templateUrl: './check-sample.component.html',
   styleUrl: './check-sample.component.css'
 })
-export class CheckSampleComponent implements OnInit, DoCheck, AfterContentChecked, AfterContentInit,AfterViewChecked, AfterViewInit {
+export class CheckSampleComponent implements OnInit, DoCheck, AfterContentChecked, AfterContentInit,AfterViewChecked, AfterViewInit, OnDestroy {
+
+  quantidade: number = 0;
 
   constructor() {}
+
+  adicionar(){
+    this.quantidade += 1;
+  }
+
+  decrementar(){
+    this.quantidade -= 1;
+  }
+
+
+  // checked -> content -> view
 
   ngDoCheck(): void {
     console.log("ngDoCheck");
   }
-  ngAfterContentChecked(): void {
-    console.log("ngAfterContentChecked");
-  }
+
+  // quando o primeiro conteúdo é iniciado
   ngAfterContentInit(): void {
     console.log("ngAfterContentInit");
   }
-  ngAfterViewChecked(): void {
-    console.log("ngAfterViewChecked");
-  }
+  // depois da inicialização da view
   ngAfterViewInit(): void {
     console.log("ngAfterViewInit");
   }
+  // após alguma alteração, verifica o conteúdo
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked");
+  }
+  // após alguma alteração, verifica a view
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewChecked");
+  }
+
   ngOnInit(): void {
     console.log("ngOnInit");
   }
 
+  ngOnDestroy(): void {
+    console.log("Goodbye my friend");
+  }
 
 }
