d7ad69669447cfaf4ee76b62b6b87c4eda358cb9
joschonarth <joschonarth@gmail.com>
2024-08-25 10:03:18 -0300

    Update Angular Rotas

--- 08-rotas/rotas-project/src/app/app-routing.module.ts
+++ 08-rotas/rotas-project/src/app/app-routing.module.ts
@@ -4,8 +4,9 @@ import { TitleComponent } from './pages/index/title/title.component';
 import { CardComponent } from './pages/portifolio/card/card.component';
 
 const routes: Routes = [
-  {path:'', component:TitleComponent, pathMatch:'full'},
-  {path:'portfolio', component: CardComponent,pathMatch:'prefix'}
+  {path:'', component: TitleComponent, pathMatch:'full'},
+  {path:'portfolio', component: CardComponent,pathMatch:'prefix'},
+  {path:'**', redirectTo:''}
 ];
 
 @NgModule({
