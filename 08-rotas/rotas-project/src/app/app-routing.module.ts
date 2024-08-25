075a421fcb2104c0921c21ddc11bc02caed12fe1
joschonarth <joschonarth@gmail.com>
2024-08-25 15:09:01 -0300

    Update Angular Rotas

--- 08-rotas/rotas-project/src/app/app-routing.module.ts
+++ 08-rotas/rotas-project/src/app/app-routing.module.ts
@@ -5,7 +5,10 @@ import { CardComponent } from './pages/portifolio/card/card.component';
 
 const routes: Routes = [
   {path:'', component: TitleComponent, pathMatch:'full'},
-  {path:'portfolio', component: CardComponent,pathMatch:'prefix'},
+  {path:'portfolio', component: CardComponent, children:[
+    {path:':id', component: CardComponent,pathMatch:'prefix'},
+    {path:':id/:token', component: CardComponent,pathMatch:'prefix'},
+  ]},
   {path:'**', redirectTo:''}
 ];
 
--- 08-rotas/rotas-project/src/app/pages/portifolio/card/card.component.ts
+++ 08-rotas/rotas-project/src/app/pages/portifolio/card/card.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
 
+  constructor(
+    private parametrizador: ActivatedRoute,
+    private navegador: Router
+  ) {
+    this.parametrizador.params.subscribe(
+      res => console.log(res)
+    )
+
+    // Params para Rotas Children
+    this.parametrizador.firstChild?.params.subscribe(
+      res => console.log(res)
+    )
+
+    this.parametrizador.queryParams.subscribe(
+      res => console.log(res)
+    )
+  }
+
+  ngOnInit(): void {
+    setInterval(()=>{
+      this.navegador.navigate(['/'])
+    }, 5000)
+  }
 }
