a3722f80cdba6ec6add33615d9dc8d880655496a
joschonarth <joschonarth@gmail.com>
2024-08-30 15:09:41 -0300

    Update Angular Guards

---
+++ 10-guards/guards-project/src/app/components/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../../services/login.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private service: LoginService){}
+
+  ngOnInit(): void {
+
+  }
+
+  logar(){
+    this.service.doLogin()
+  }
+
+  deslogar(){
+    this.service.doLogout()
+  }
+
+}
---
+++ 10-guards/guards-project/src/app/guards/auth.guard.ts
@@ -0,0 +1,7 @@
+import { CanActivateFn } from '@angular/router';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const hasToken = !!localStorage.getItem('token')
+
+  return hasToken
+};
